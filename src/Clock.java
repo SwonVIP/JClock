c7d45b82921b595b70b8da25b422f158e08b4f66
Sven.Scheffel <sven.scheffel@de.ebmpapst.com>
2016-05-04 10:57:02 +0200

    Label Array gefüllt mit Buchstaben, zeigt verschiedene Ziffern auf GUI an

--- src/Clock.java
+++ src/Clock.java
@@ -1,69 +1,255 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.border.LineBorder;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
-public class Clock {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+public class Clock extends JFrame {
 	
-    static String[] data = { "ESKISCHAFÜF", "VIERTUBFZÄÄ", "ZWÄNZGSIVOR", "ABOHAUBIEPM",  
-    						 "EISZWÖISDRÜ", "VIERIFÜFIGT", "SÄCHSISIBNI", "ACHTINÜNIEL", 
-    						 "ZÄNIERBEUFI", "ZWÖUFIAMUHR" };
+//	Clock o = new Clock();
+	
+	Date date = new Date();
+	JPanel panel = new JPanel();
+	JLabel[] numbers = new JLabel[200];
+	
+    static String[] data = { "ESKISTAFÜNF", "ZEHNZWANZIG", "DREIVIERTEL", "VORFUNKNACH",  
+    						 "HALBAELFÜNF", "EINSXAMZWEI", "DREIPMJVIER", "SECHSNLACHT", 
+    						 "SIEBENZWÖLF", "ZEHNEUNKUHR" };
+    
+
+	public Clock()
+	{
+		panel.setBorder(javax.swing.BorderFactory.createEmptyBorder(20, 50, 20, 50));
+		panel.setLayout(new GridLayout(10,11));
+		panel.setBackground(Color.black);
+		add(panel);
+		
+		String input;
+		int o = 0;
+		
+		for(int i=0;i<10;i++)
+		{
+			for(int n=0;n<11;n++)
+			{
+				
+				input = (data[(i)].charAt(n) + "");
+				
+				System.out.println(input);
+
+				numbers[o] = new JLabel(input);
+			    numbers[o].setForeground(Color.gray);
+			    panel.add(numbers[o]);
+			    o++;
+			}
+
+		}
+		
+	}
+	
+//    public void getState()
+//    {
+//    	//fuenf
+//    	
+////    	switch(fuenf)
+////    	{
+////    	
+////    	case 5:
+////    	
+////    	}
+//    }
+    
+    
+    
 	
 	public static void main(String[] args) {
-		JFrame mainClock = new JFrame();
-		mainClock.setTitle("Main menu");
-		mainClock.setSize(800, 800);
-		mainClock.setVisible(true);
+		//Hauptmenü Prototyp
+//		JFrame mainClock = new JFrame();
+//		mainClock.setTitle("Main menu");
+//		mainClock.setSize(800, 800);
+//		mainClock.setVisible(true);
+//
+//		JButton buttonSelectS= new JButton("Schweizer Uhr");
+//		buttonSelectS.setBounds(0, 0, 300, 100);
+//		
+//		JButton buttonSelectFR = new JButton("Französische Uhr");
+//		buttonSelectFR.setBounds(310, 110, 300, 100);
+//		
+//		mainClock.add(buttonSelectS);
+//		mainClock.add(buttonSelectFR);
+		
+		Clock sClock = new Clock();
+		sClock.setTitle("Schweizer Uhr");	
+		sClock.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		sClock.pack();
+		sClock.setSize(700,700);
+		sClock.setVisible(true);
+		sClock.getContentPane().setBackground(Color.BLACK);
+		
+	    new javax.swing.Timer(1000,
+	    		  new ActionListener() {
+	    		  	public void actionPerformed(ActionEvent arg0) {
+	    		  	sClock.update(sClock);
+	    		  }
+	    		  }
+	    		  ).start();
 
-		JButton buttonSelectS= new JButton("Schweizer Uhr");
-		buttonSelectS.setBounds(0, 0, 300, 100);
-		
-		JButton buttonSelectFR = new JButton("Französische Uhr");
-		buttonSelectFR.setBounds(310, 110, 300, 100);
-		
-		mainClock.add(buttonSelectS);
-		mainClock.add(buttonSelectFR);
+	    
+	    
+	    
 		
 		init();
 		
 	}
-	public static void init()
+	//Neun zeichnen und aktivierte/benötigte Label färben
+	public void redraw()
 	{
-		JFrame sClock = new JFrame();
-		sClock.setTitle("Schweizer Uhr");
-		sClock.setSize(500, 500);
-		sClock.setVisible(true);
-		sClock.getContentPane().setBackground(Color.BLACK);
+		String input;
 		
-        JTable jtable = new JTable( 4, 1  );
-        jtable.setBorder( new LineBorder( Color.white, 3 ));
-        jtable.setRowHeight( 20 );
-        sClock.add( jtable, BorderLayout.CENTER );
-        
-        
-		
-		String test;
-		
-		for(int i=1;i<9;i++)
+		for(int i=0;i<10;i++)
 		{
-			for(int n=0;n<10;n++)
+			for(int n=0;n<11;n++)
 			{
-//				test = (data[(i)].charAt(n) + "");
-				test = (data[(i)]);
-			    JLabel numbers = new JLabel(test, 0);
-			    numbers.setForeground(Color.white);
-			    sClock.add(numbers);
+				input = (data[(i)].charAt(n) + "");
+				
+				System.out.println(input);
+
+			    JLabel numbers = new JLabel(input);
+			    numbers.setForeground(Color.gray);
+			    panel.add(numbers);
 			}
 
 		}
+	}
+    
+
+	public static void init()
+	{
+		//Uhrzeit holen
+		
+		Calendar c = new GregorianCalendar();
+		int hour = c.get(Calendar.HOUR);
+		int minute = c.get(Calendar.MINUTE);
+		int second = c.get(Calendar.SECOND);
+		
+		System.out.println(hour + "/" + minute + "/" + second + "/");
+		
+		//Logik Uhr
+		
+		//1;5 nach 1; 10 nach 1; 15 nach 1; 20 nach 1; 5 vor halb 2; halb 2; 5 nach halb 2; 20 vor 2; 15 vor 2; 10 vor 2; 5 vor 2; 2
+		
+		
+		//Uhr aktualisieren, jede Minute aktualisieren
+
+		
+		String test;
+		
+
 		
 		//test123
 
 
 	}
+	public void update(Clock sClock)
+	{
+		   Calendar c =  new GregorianCalendar();
+		   int hr = c.get(Calendar.HOUR_OF_DAY);
+		   int mn = c.get(Calendar.MINUTE);
+		   
+		   //Minuten
+		   
+		    switch (mn)
+		    {
+		    case 0: 
+
+		      break;
+		    case 5: 
+
+		      break;
+		    case 10: 
+		      break;
+		    case 15: 
+
+		      break;
+		    case 20: 
+
+		      break;
+		    case 25: 
+
+		      break;
+		    case 30: 
+
+		      break;
+		    case 35: 
+
+		      break;
+		    case 40: 
+
+		      break;
+		    case 45: 
+
+		      break;
+		    case 50: 
+
+		      break;
+		    case 55: 
+
+		      break;
+		    }
+		    switch (hr)
+		    {
+		    case 1: 
+
+		      break;
+		    case 2: 
+
+		      break;
+		    case 3: 
+
+		      break;
+		    case 4: 
+
+		      break;
+		    case 5: 
+
+		      break;
+		    case 6: 
+
+		      break;
+		    case 7: 
+
+		      break;
+		    case 8: 
+
+		      break;
+		    case 9: 
+
+		      break;
+		    case 10: 
+
+		      break;
+		    case 11: 
+
+		      break;
+		    case 12: 
+
+		      break;
+		    }
+
+	}
+
 }
 
---
+++ src/Time.java
@@ -0,0 +1,16 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Time {
+
+	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	Date date = new Date();
+	
+//	public  void main(String[] args) {
+//		
+//		System.out.println(dateFormat.format(date)); //2014/08/06 15:59:48
+//		
+//	}
+}
+	
